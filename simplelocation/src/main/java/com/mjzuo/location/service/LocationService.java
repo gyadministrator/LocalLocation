8da7816c4c6f1294697e6fb06ba3acdc7faa019d
liguixing <lgx.8888@qq.com>
2020-12-23 16:22:30 +0800

    Initial commit

---
+++ simplelocation/src/main/java/com/mjzuo/location/service/LocationService.java
@@ -0,0 +1,125 @@
+package com.mjzuo.location.service;
+
+import android.annotation.SuppressLint;
+import android.app.Service;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.google.gson.Gson;
+import com.mjzuo.location.GeocodingManager;
+import com.mjzuo.location.ReverseGeocodingManager;
+import com.mjzuo.location.bean.Latlng;
+import com.mjzuo.location.constant.Constant;
+import com.mjzuo.location.location.GoogleGeocoding;
+import com.mjzuo.location.location.IGeocoding;
+import com.mjzuo.location.regelocation.IReGe;
+
+/**
+ * @ProjectName: SimpleLocation-master
+ * @Package: com.mjzuo.location.service
+ * @ClassName: LocationService
+ * @Author: 1984629668@qq.com
+ * @CreateDate: 2020/12/23 14:22
+ */
+public class LocationService extends Service {
+    /**
+     * 定位获取经纬度，包括LocationManager、基站地位
+     */
+    GeocodingManager siLoManager;
+    /**
+     * 反地理编码的manager，包括google反地理、高德反地理、百度反地理、腾讯反地理
+     */
+    ReverseGeocodingManager reGeManager;
+
+    private static final String TAG = "LocationService";
+
+    private double lat;
+    private double lng;
+
+    public static final String LOCATION_TAG = "location_tag";
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.e(TAG, "onCreate: 开始定位...");
+        init();
+    }
+
+    private void init() {
+        ReverseGeocodingManager.ReGeOption reGeOption = new ReverseGeocodingManager.ReGeOption()
+                .setReGeType(Constant.TENCENT_API)// 腾讯api返地理编码
+                .setSn(true)// sn 签名校验方式
+                .setIslog(true);// 打印log
+        reGeManager = new ReverseGeocodingManager(this, reGeOption);
+        reGeManager.addReGeListener(new IReGe.IReGeListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onSuccess(int state, Latlng latlng) {
+                latlng.setLatitude(lat);
+                latlng.setLongitude(lng);
+                SharedPreferences sharedPreferences = getSharedPreferences(LOCATION_TAG, MODE_PRIVATE);
+                SharedPreferences.Editor edit = sharedPreferences.edit();
+                String s = new Gson().toJson(latlng);
+                edit.putString(LOCATION_TAG, s);
+                edit.apply();
+                Log.e(TAG, "reGeManager onSuccess:" + latlng.toString());
+            }
+
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onFail(int errorCode, String error) {
+                Log.e(TAG, "onFail: " + "errorCode:" + errorCode + ", error:" + error);
+                init();
+            }
+        });
+
+
+        GeocodingManager.GeoOption option = new GeocodingManager.GeoOption()
+                .setGeoType(Constant.BS_OPENCELLID_API) // 使用openCellid服务器的基站地位
+                .setOption(new GoogleGeocoding.SiLoOption()
+                        .setGpsFirst(false));// locationManager定位方式时，gps优先
+        siLoManager = new GeocodingManager(this, option);
+        siLoManager.start(new IGeocoding.ISiLoResponseListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onSuccess(Latlng latlng) {
+                Log.e(TAG, "siLoManager onSuccess:" + latlng.toString());
+                lat = latlng.getLatitude();
+                lng = latlng.getLongitude();
+                reGeManager.reGeToAddress(latlng);
+            }
+
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onFail(String msg) {
+                Log.e(TAG, "error:" + msg);
+                init();
+            }
+        });
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (siLoManager != null)
+            siLoManager.stop();
+        if (reGeManager != null) {
+            reGeManager.stop();
+        }
+    }
+}
---
+++ simplelocationdemo/src/main/java/com/mjzuo/location/demo/LocationActivity.java
@@ -0,0 +1,85 @@
+package com.mjzuo.location.demo;
+
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.gson.Gson;
+import com.mjzuo.location.bean.Latlng;
+import com.mjzuo.location.service.LocationService;
+import com.mjzuo.location.util.LocationUtil;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class LocationActivity extends AppCompatActivity {
+    TextView tvSimpleLo;
+    TextView tvSimpleAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.simple_location_activity);
+
+        tvSimpleLo = findViewById(R.id.tv_simple_location_txt);
+        tvSimpleAd = findViewById(R.id.tv_simple_address_txt);
+
+        LocationUtil.startLocation(this);
+
+        //启动定时器
+        timer.schedule(task, 0, 1000);
+    }
+
+    private void initData() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                SharedPreferences sharedPreferences = getSharedPreferences(LocationService.LOCATION_TAG, MODE_PRIVATE);
+                String s = sharedPreferences.getString(LocationService.LOCATION_TAG, "");
+                Log.e("result", "run: " + s);
+                if (!TextUtils.isEmpty(s)) {
+                    Latlng latlng = new Gson().fromJson(s, Latlng.class);
+                    if (latlng != null) {
+                        tvSimpleAd.setText(latlng.toString());
+                    }
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        LocationUtil.onDestroy();
+    }
+
+
+    @SuppressLint("HandlerLeak")
+    private Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (msg.what == 1) {
+                initData();
+            }
+        }
+    };
+
+
+    private Timer timer = new Timer(true);
+
+    //任务
+    private TimerTask task = new TimerTask() {
+        public void run() {
+            Message msg = new Message();
+            msg.what = 1;
+            handler.sendMessage(msg);
+        }
+    };
+}
